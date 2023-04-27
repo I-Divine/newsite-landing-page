ae6dcf2b039e47f308b334edf4b1340259cc114b
I_Divine <64442300+I-Divine@users.noreply.github.com>
2023-04-27 02:39:17 +0100

    Add files via upload

---
+++ newsite-landing-page/README.md
@@ -0,0 +1,3 @@
+# newsite-landing-page
+landing page or a newsite
+made with html and css
---
+++ newsite-landing-page/news-homepage-main/index.js
@@ -0,0 +1,8 @@
+
+const element = document.getElementById("readMore-container");
+element.onmouseout= ()=>{
+    element.style.backgroundColor = "hsl(240, 100%, 5%)"
+}
+element.onmouseover= ()=>{
+    element.style.backgroundColor = "hsl(5, 85%, 63%)";
+}
